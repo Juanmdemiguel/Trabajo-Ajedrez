048bf1ab00268c6ef1ccacc75250ddfe936e606e
DiegoRodriguezGonzalez <diego.rgonzalez@alumnos.upm.es>
2024-04-12 19:40:11 +0200

    Código ETSIDI
    Se incluye una imagen

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,24 +1,19 @@
 #include "freeglut.h"
-#include "assimp/aabb.h"		//Comprobacion correcta instalacion assimp
 #include "ETSIDI.h"
 
-
-//los callback, funciones que seran llamadas automaticamente por la glut
-//cuando sucedan eventos
 //NO HACE FALTA LLAMARLAS EXPLICITAMENTE
-void OnDraw(void);		 //esta funcion sera llamada para dibujar
+void OnDraw(void); //esta funcion sera llamada para dibujar
 void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
 void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
 
-
-int main(int argc, char* argv[]) //Primero numero de caracteres, luego array
+int main(int argc, char* argv[])
 {
 	//Inicializar el gestor de ventanas GLUT
 	//y crear la ventana
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 600);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("Ajedrez");
+	glutCreateWindow("MiJuego");
 
 	//habilitar luces y definir perspectiva
 	glEnable(GL_LIGHT0);
@@ -29,62 +24,62 @@ int main(int argc, char* argv[]) //Primero numero de caracteres, luego array
 	gluPerspective(40.0, 800 / 600.0f, 0.1, 150);
 
 	//Registrar los callbacks
-	glutDisplayFunc(OnDraw); //Cuando tiene que dibujar llama OnDraw
+	glutDisplayFunc(OnDraw);
 	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
-	glutKeyboardFunc(OnKeyboardDown); //Cuando se pulsa un botón llama OnKeyboardDown
-	//pasarle el control a GLUT,que llamara a los callbacks
-	glutMainLoop();	//Juego
+	glutKeyboardFunc(OnKeyboardDown);
 
+	//POSIBLE INICIALIZACION
+
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();
 
 	return 0;
 }
+
 void OnDraw(void)
 {
 	//Borrado de la pantalla	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Se borra buffer de color y de profuncidad
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Para definir el punto de vista
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity(); //Se carga la matrizidentidad para situarse en un punto referencia. Los objetos se pondrán en ese punto de referencia
+	glLoadIdentity();
 
-	gluLookAt(0.0, 10.0, 20.0,  //pone posición a la cámara
-		0.0, 0.0, 0.0,      // hacia que punto mira  (0,0,0) 
-		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
+	//funciones de dibujo
 
-	//aqui es donde hay que poner el código de dibujo
-	//Plano de colores de lado 10
+	gluLookAt(0, 7.5, 20,  // posicion del ojo
+		0.0, 7.5, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)  
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo.png").id);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
-	glColor3ub(255, 0, 0);
-	glVertex3f(-5.0f, 0, -5.0f);
-	glColor3ub(150, 100, 0);
-	glVertex3f(-5.0f, 0, 5.0f);
-	glColor3ub(200, 0, 150);
-	glVertex3f(5.0f, 0, 5.0f);
-	glColor3ub(255, 200, 100);
-	glVertex3f(5.0f, 0, -5.0f);
+	glColor3f(1, 1, 1);
+	glTexCoord2d(0, 1); glVertex2f(-10, 0);
+	glTexCoord2d(1, 1); glVertex2f(10, 0);
+	glTexCoord2d(1, 0); glVertex2f(10, 15);
+	glTexCoord2d(0, 0); glVertex2f(-10, 15);
 	glEnd();
 	glEnable(GL_LIGHTING);
+	glDisable(GL_TEXTURE_2D);
 
 	//no borrar esta linea ni poner nada despues
 	glutSwapBuffers();
 }
-
-void OnKeyboardDown(unsigned char key, int x_t, int y_t) //Evento de teclado
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
 {
-	
+	//poner aqui el código de teclado
+
 
 	glutPostRedisplay();
 }
 
-
-void OnTimer(int value) //Evento de tiempo
+void OnTimer(int value)
 {
 	//poner aqui el código de animacion
-	
 
-	//no borrar estas lineas
+
+		//no borrar estas lineas
 	glutTimerFunc(25, OnTimer, 0);
 	glutPostRedisplay();
-
 }
