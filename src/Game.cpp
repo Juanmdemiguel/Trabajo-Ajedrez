b6cbe74b262089a122b9673b74d9484e1e95c43c
DiegoRodriguezGonzalez <diego.rgonzalez@alumnos.upm.es>
2024-05-04 15:23:31 +0200

    Se dibujan las piezas en el tablero
    Falta ver por qué no se dibujan las piezas en las dos últimas columnas

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,17 +1,12 @@
 #include"Game.h"
 
 void Game::dibujaJuego(piece piezas) {	//Funcion dibujaJuego provisional, se añade a clase Juego/Game cuando se desarrolle
-
+	
+	//Dibuja el tablero
 	board.dibuja();
-	glPushMatrix();
-	//glTranslatef(board.getTile({ 7,6 }).getCenter().x, 0.0, board.getTile({ 7,6 }).getCenter().z); 
-	glScalef(0.05f, 0.05f, 0.05f);
-	glRotatef(-90, 90.0f, 0.0f, 1.0f);
-	glColor3f(0.1f, 0.1f, 0.1f);
-	piezas.dibujaPiezas(board);
-	glPopMatrix();
-	glColor3f(0, 0, 0);
 
+	//Dibuja todas las piezas
+	piezas.dibujaPiezas(board);
 }
 
 void Game::musica(bool sonido, int tema)
--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,6 +1,5 @@
 #include "Piece.h"
 #include "freeglut.h"
-#include <iostream>
 
 void piece::elige(TipoPieza p, Punto2D posicion)
 {
@@ -42,48 +41,59 @@ void piece::elige(TipoPieza p, Punto2D posicion)
 }
 void piece::dibuja1Pieza(Punto2D posicion, Model modelo)
 {
+	glPushMatrix();
 	glTranslatef(posicion.x, 0.0, posicion.z);
+	glScalef(0.05f, 0.05f, 0.05f);
+	glRotatef(-90, 90.0f, 0.0f, 1.0f);
+	glColor3f(0.1f, 0.1f, 0.1f);
 	modelo.Draw();
-	glTranslatef(-posicion.x, 0.0,-posicion.z);
+	glPopMatrix();
+	glColor3f(0, 0, 0);
 }
 
 void piece::dibujaPiezas(Board tablero)
 {
 	//Dibujo de los peones
-	for (double i = 1.0; i<= fil - 2; i += (fil - 3))
+	for (double i = 2; i <= fil - 1; i += (fil - 3))
 	{
-		for (double j = 0.0; j < col; j++)
+		for (double j = 1; j <= col; j++)
 		{
-			this->dibuja1Pieza({tablero.getTile({5,8}).getCenter()}, Pawn);
+			this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
 		}
 	}
 
-	/*
 	//Dibujo del resto de piezas
-	for (double i = 0; i <= fil-1; i += (fil-2))
+	for (double i = 1; i <= fil; i += (fil-1))
 	{
-		for (double j = 0; j < col; j++)
+		for (double j = 1; j <= col; j++)
 		{
 			//Dibujo de las torres
-			if (j==0 || j==col-1)
-			this->dibuja1Pieza({ square * i,square * j }, Pawn);
-
+			if (j == 1 || j == col)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
+		
 			//Dibujo de los caballos
-			if (j == 1 || j == col - 2)
-				this->dibuja1Pieza({ square * i,square * j }, Pawn);
+			if (j == 2 || j == col - 1)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
+
+			//Dibujo del arzobispo
+			if (j == 3)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
 
 			//Dibujo de los alfiles
-			if (j == 2 || j == col - 3)
-				this->dibuja1Pieza({ square * i,square * j }, Pawn);
+			if (j == 4 || j == col - 3)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
 
-			//Dibujo del rey
-			if (j == 3)
-				this->dibuja1Pieza({ square * i,square * j }, Pawn);
+			//Dibujo del canciller
+			if (j == 8)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
 
 			//Dibujo de la reina
-			if (j == 4)
-				this->dibuja1Pieza({ square * i,square * j }, Pawn);
+			if (j == 5)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
+
+			//Dibujo del rey
+			if (j == 6)
+				this->dibuja1Pieza(tablero.getTile({ j,i }).getCenter(), Pawn);
 		}
 	}
-	*/
 }
