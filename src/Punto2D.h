05e497ed725ee49f4f3c3e1d210ab4917f98a9cd
IvanAsensioDiez <ivan.asensio@alumnos.upm.es>
2024-05-05 16:57:46 +0200

    Función cronometro
    Añade la función cronometro a Punto2D

--- src/Punto2D.h
+++ src/Punto2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <ctime>
 using namespace std;
 
 class Punto2D
@@ -14,3 +15,4 @@ float map(float x, float in_min, float in_max, float out_min, float out_max); //
 float map2(vector<vector<double>> matriz, float x);//Escala cuadraticamente una variable en un rango de valores
 void drawEsferadePruebas(float sphereX, float sphereY);//Dibuja una esfera roja en unas coordenadas (se debe borrar cuando queramos)
 double modulo(Punto2D, Punto2D);
+double cronometro(int x);//0 para reiniciar cornometro, 1 para tomar medida desde inicio, 2 para tomar medida desde la anterior medida
